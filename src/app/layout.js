17906709e9154a9b178d60a8bc5241f7a4b5fb58
Swapnabir Dutta <swapnabirdutta@Swapnabirs-MacBook-Air.local>
2024-12-09 13:34:59 +0530

    added metadata

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,6 +2,12 @@ import './globals.css';
 import Navbar from '../components/Navbar';
 import ScrollIndicator from '../components/ScrollIndicator';
 
+
+export const metadata = {
+  title: 'Swapnabir Dutta,',
+  description: 'Portfolio of Swapnabir Dutta,',
+}
+
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
